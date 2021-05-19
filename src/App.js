bc0c4b608b3a1d98b07998381f634cf30e03109a
Tamara <tabualhsan@gmail.com>
2021-05-18 17:03:30 -0700

    set up API setup in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(props) {
+
+  const [items, setitems] = useState([]);
+  const [isLoaded, setisLoaded] = useState(false); 
+
+  useEffect(() => {
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(json => {
+      setitems(json);
+      
+      setisLoaded(true); 
+    })
+  }, [items])
+  if (!isLoaded){
+    return <div> Loading...</div>
+  }
+  return(
+    <div >
+      <ul>      
+        {items.map(item =>(
+          <li key={item.id}>
+            {item.name}
+          </li>
+        ))};   
+      </ul>     
     </div>
-  );
+  ) 
 }
+
+
+// class App extends Component{
+
+//   constructor(props){
+//     super(props);
+//     this.state = {
+//       items: [],
+//       isLoaded: false,
+
+//     }
+//   }
+//   componentDidMount(){
+
+//     // api call 
+//     // get and save info in a state from API
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//       .then(res => res.json())
+//       .then(json => {
+//         this.setState({
+//           isLoaded: true,
+//           items: json,
+
+//         })
+
+//     });
+//   }
+//   render(){
+//     var { isLoaded, items } = this.state;
+//     if (!isLoaded){
+//       return <div> Loading...</div>
+
+//     }
+  
+//     else{
+    
+    
+//       return (
+//         <div >
+//           <ul>
+            
+//           {items.map(item =>(
+//             <li key={item.id}>
+//               {item.name}
+
+//             </li>
+
+//           ))};
+
+//           </ul>
+      
+
+//       </div>
+//     );
+//   }}}
+  
 
 export default App;
